6ece274063301bc7cc8a4c62050d6d696eb6b276
adistars <adistars@gmail.com>
2016-11-15 11:14:04 -0800

    string compression question

--- Chap1/chap1qs.py
+++ Chap1/chap1qs.py
@@ -2,6 +2,24 @@ import sys
 
 
 def main():
+    count = 1
+    new = []
+    strng = 'abc'#cccaaa'
+    for i in xrange(len(strng)-1):
+        if strng[i] == strng[i+1]:
+            count+=1
+        else:
+            new.append(strng[i])
+            new.append(count)
+            count = 1
+    new.append(strng[i+1])
+    new.append(count)
+    print new
+    for i in xrange(0,len(new),2):
+        if new[i+1]!=1:
+            return new
+
+'''
     if len(sys.argv)<2:
         print "Not input provided"
         sys.exit(0)
@@ -15,7 +33,7 @@ def main():
         else:
             print "duplicate: " + i
 
-'''
+
     #hash table implementation
     for i in range(0,len(input_string)):
         try:
